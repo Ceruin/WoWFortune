e170b74002db5ea347727a19c5d38e1ab142310d
Ceruin <13532471+Ceruin@users.noreply.github.com>
2024-03-04 01:50:12 -0500

    Revert "."
    This reverts commit 98f85e22587d19fca6dc1dd63f8d048143fb89aa.

--- script.js
+++ script.js
@@ -6,20 +6,24 @@ $(document).ready(function() {
     function generateCopyPasta(userInput) {
         const words = userInput.split(/\s+/); // Tokenize user input
         let copyPasta = "";
-        
-        // Iterate through user input words
+
         words.forEach(word => {
-            // Use user input if it matches a category
-            if (vocabulary.hasOwnProperty(word)) {
-                const options = vocabulary[word];
-                const choice = options[Math.floor(Math.random() * options.length)];
-                copyPasta += replaceWithSynonyms(choice) + " ";
-            } else {
-                // Otherwise, select a random word from the predefined categories
-                const randomCategory = Object.values(vocabulary)[Math.floor(Math.random() * Object.keys(vocabulary).length)];
-                const randomWord = randomCategory[Math.floor(Math.random() * randomCategory.length)];
-                copyPasta += replaceWithSynonyms(randomWord) + " ";
-            }
+            // Find the closest word from predefined categories
+            let closestWord = "";
+            let minDistance = Infinity;
+
+            Object.values(vocabulary).forEach(category => {
+                category.forEach(item => {
+                    const [wordInCategory, japaneseWord] = item.split(' ');
+                    const distance = Math.abs(word.localeCompare(wordInCategory, 'en', { sensitivity: 'base' }));
+                    if (distance < minDistance) {
+                        closestWord = japaneseWord ? japaneseWord : wordInCategory;
+                        minDistance = distance;
+                    }
+                });
+            });
+
+            copyPasta += closestWord + " ";
         });
 
         return copyPasta.trim();
